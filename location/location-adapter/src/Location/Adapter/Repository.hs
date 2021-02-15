1430e9faddefeb97c00326c6b3c69590dce40483
Will Leyton <wleylop22@gmail.com>
2021-02-15 09:37:57 -0500

    Add LocationRepository module

---
+++ location/location-adapter/src/Location/Adapter/Repository.hs
@@ -0,0 +1,26 @@
+module Location.Adapter.Repository (LocationRepository (..)) where
+
+import Core.Dsl
+  ( Drone (..),
+    Position (..),
+    eval,
+  )
+import qualified Files.File as F
+import Location.Adapter.Config.LocationConfig
+  ( LocationConfig (..),
+    fromEnv,
+  )
+import Location.Domain.Repository (LocationRepository (..))
+
+instance LocationRepository IO where
+  save (Drone n []) = pure ()
+  save drone = do
+    config <- fromEnv
+    let dir = F.Directory $ outPath config
+    F.write (droneToFile config drone) dir
+
+droneToFile :: LocationConfig -> Drone -> F.File
+droneToFile config (Drone n cmds) =
+  F.File
+    (F.Filename (prefix config <> n <> extension config))
+    (F.Line . show . eval <$> cmds)
