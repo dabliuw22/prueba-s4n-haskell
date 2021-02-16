374b40839fa4bcbbc6e1f138b6a032865442665f
Will Leyton <wleylop22@gmail.com>
2021-02-16 09:29:10 -0500

    Add typeclass LocationService

--- location/location-application/src/Location/Application/Location.hs
+++ location/location-application/src/Location/Application/Location.hs
@@ -1,7 +1,11 @@
-module Location.Application.Location (save) where
+module Location.Application.Location (LocationService (save)) where
 
 import Core.Dsl (Drone (..))
 
-save :: (Drone -> IO ()) -> [Drone] -> IO ()
-save f drones' = do
-  sequence_ (f <$> drones')
+class Monad m => LocationService m where
+  save :: (Drone -> m ()) -> [Drone] -> m ()
+
+instance LocationService IO where
+  -- save :: (Drone -> IO ()) -> [Drone] -> IO ()
+  save f drones' = do
+    sequence_ (f <$> drones')
